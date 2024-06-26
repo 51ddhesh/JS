9c0974deeeeb25fdbd5270862efb16349abb1943
Siddhesh Badnapurkar <siddheshbadnapurkar@gmail.com>
2024-06-26 17:25:21 +0000

    added in a few more variables

--- 01/variables.js
+++ 01/variables.js
@@ -4,3 +4,15 @@ console.log(id);
 id = 4321 won't work as the variable id is of const type
 we cannot modify it further
 */
+let email = "username@mail.com"
+let passkey = "password"
+city = "New Delhi"
+//we can declare variables as above
+
+console.table([id, email, passkey, city])
+//print in form a table
+
+let state;
+//this will create a space in the memory
+//on printing this variable we will get "undefined"
+console.log(state);
